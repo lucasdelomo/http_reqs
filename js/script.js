6edee998f17084f6181a5ffdb639456b8950d189
Lucas Delomo Pereira <lucasdelomo@gmail.com>
2021-01-02 13:57:47 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+const json =  {"senha": "A SENHA DESEJADA"};
+const options = {
+    method: 'POST',
+    body: JSON.stringify(json),
+    headers: {
+		
+        'Content-Type': 'text/plain'
+    }
+}
+
+fetch('https://webhook.site/f5000310-e98c-48ec-9867-5916fefe93ff', options)
+   .then(res => res.json())
+   .then(res => console.log(res))
+  .catch(err => console.error(err));	
+   
